9b3056676cf6b9f423edd0eb2c5e7f446d19f5ec
Oracle Public Cloud User <opc@web.subnet04262033.vcn04262033.oraclevcn.com>
2025-04-28 16:54:44 +0000

    gh-pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/web2-25t-rookiehu01/',
   plugins: [react()],
   server: {
     host: true,
