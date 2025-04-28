d3d5eed0c40387acdec1293018f14a5d8f8e53b6
Oracle Public Cloud User <opc@web.subnet04262033.vcn04262033.oraclevcn.com>
2025-04-28 13:57:10 +0000

    changed files to be able to host it publically maybe

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
+    allowedHosts: ['rookiehu.ddns.net'],
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
