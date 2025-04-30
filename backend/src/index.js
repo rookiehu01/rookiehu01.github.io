c95b2fe1257286e20b87fcc895f94df6b51633a7
Attila Markó <P26LF3@uni-sopron.hu>
2025-04-30 13:08:00 +0200

    ??

--- backend/src/index.js
+++ backend/src/index.js
@@ -99,9 +99,7 @@ mongoose.connection.once("open", async () => {
 const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(cors({
-  origin: 'https://www.rookiehu.me'
-}));
+app.use(cors());
 
 app.use('/uploads', express.static('uploads'))
 app.use('/public', express.static('public'))
